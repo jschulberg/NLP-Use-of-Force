8cab1d22325f68c7d4dfc304bb16e2c532c622f0
Justin Schulberg <schulbergjustin@gmail.com>
2020-09-21 19:39:22 -0400

    Managed to read in my dataset (working on Baltimore.txt) and split the text successfully.

--- Code/NLP Analysis Script.py
+++ Code/NLP Analysis Script.py
@@ -5,10 +5,18 @@ Created on Tue Sep 15 17:57:08 2020
 @author: jschulberg
 """
 
-# Import packages
+######################################
+###        Import packages         ###
+######################################
 import os # Helps work with directories
+import nltk # Helps with NLP work
+from collections import Counter # Helps with word counts
 
-### Read in data
+
+
+######################################
+###           Read in Data         ###
+######################################
 # Change the working directory
 os.chdir(r'C:\Users\jschulberg\Documents\Learning\NLP\NLS Mentorship Program\NLP-Use-of-Force\Data')
 os.getcwd()
@@ -26,4 +34,21 @@ os.listdir()
 # print(files)
 
 baltimore_data = open("Baltimore.txt", "r")
-print(f.read())
+
+# Read the lines of the file
+baltimore_txt = baltimore_data.readlines()
+print(baltimore_txt)
+
+type(baltimore_txt) # Our data is in list format
+
+######################################
+###            Word Count          ###
+######################################
+# Split the data
+baltimore_split = baltimore_txt[0].split()
+
+######################################
+###            Word Count          ###
+######################################
+# Let's loop through our dataset and count everything using counter
+Counter(word.lower() for word in baltimore_split)
